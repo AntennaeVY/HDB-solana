2f52217638eb424ab3af7e48ed2c5d69a5859688
AntennaeVY <hypernovavy1995@gmail.com>
2024-02-14 14:35:32 -0400

    feat: :sparkles: show SILLY amount
    Displaying the amount of SILLY tokens associated with the connected wallet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterModule } from '@angular/router';
+import { WalletStore } from '@heavy-duty/wallet-adapter';
 import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
+import { computedAsync } from 'ngxtension/computed-async';
+import { ShyftApiService } from './shyft-api.service';
 
 @Component({
   standalone: true,
@@ -13,9 +17,27 @@ import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-materia
       <div class="flex justify-center">
         <hd-wallet-multi-button></hd-wallet-multi-button>
       </div>
+
+      @if (account()) {
+        <div
+          class="absolute top-4 left-4 flex justify-center items-center gap-2"
+        >
+          <img [src]="account()?.info?.image" class="w-8 h-8" />
+          <p class="text-xl">{{ account()?.balance }}</p>
+        </div>
+      }
     </header>
   `,
 })
 export class AppComponent {
-  title = 'solana-bootcamp';
+  private readonly _shyftApiService = inject(ShyftApiService);
+  private readonly _walletStore = inject(WalletStore);
+  private readonly _publicKey = toSignal(this._walletStore.publicKey$);
+
+  readonly account = computedAsync(
+    () => this._shyftApiService.getAccount(this._publicKey()?.toBase58()),
+    { requireSync: true },
+  );
+
+  title = 'HDB in da house 😎';
 }
